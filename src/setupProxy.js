edb6d655c864e7ea1542e850f07d02a1d6e22df0
13982720426 <1679575151@qq.com>
2021-03-08 20:03:36 +0800

    配置本地环境变量

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,11 +2,12 @@ const proxy = require('http-proxy-middleware')
 
 module.exports = function (app) {
   app.use(
-    proxy('/devApi', {
-      target: 'http://www.web-jshtml.cn/api/react', //配置要请求的服务器地址
+      //配置本地环境变量 地址从.env.development 中获取
+    proxy([process.env.REACT_APP_API], {
+      target: process.env.REACT_APP_BASE_URL, //配置要请求的服务器地址
       changeOrigin: true,
       pathRewrite: {
-        '^/devApi': '',
+        [`^${process.env.REACT_APP_API}`]: '',
       },
     })
   )
