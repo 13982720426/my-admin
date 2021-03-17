8a7261ebb0f20ca58c5457e47e13a1e0781fc91c
13982720426 <1679575151@qq.com>
2021-03-17 21:43:02 +0800

    table组件第三次封装，父组件调用子组件方法，封装删除、批量删除功能

--- src/api/common.js
+++ src/api/common.js
@@ -11,3 +11,14 @@ export function TableList(params) {
     // params: data, //请求类型为 get时
   })
 }
+/**
+ * 删除列表
+ */
+export function TabaDelete(params) {
+  return service.request({
+    url: params.url,
+    method: params.method || 'post',
+    data: params.data, //请求类型为 post时
+    // params: data, //请求类型为 get时
+  })
+}
